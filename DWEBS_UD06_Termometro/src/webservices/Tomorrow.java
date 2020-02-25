df8ac1a1b907c2e92fdf0944c50df4a5256415f2
helenamoreda <helena1094@gmail.com>
2020-02-25 08:29:37 +0100

    Update Tomorrow.java

--- DWEBS_UD06_Termometro/src/webservices/Tomorrow.java
+++ DWEBS_UD06_Termometro/src/webservices/Tomorrow.java
@@ -8,7 +8,16 @@ import javax.ws.rs.PathParam;
 public class Tomorrow {
 	@GET
 	@Path("{ciudad}")
-	public String mostrarCodigo(@PathParam("ciudad") String ciudad) {
-		return "GET: Nombre: ";
+	public String mostrarCodigo(@PathParam("ciudad") String ciudad, @QueryParam("format") String format) throws MalformedURLException, IOException, ParseException {
+		Util u = new Util();
+		
+		if (format.equals("json")) {
+			u.buscaProvincia(ciudad);
+		} else if (format.equals("xml")) {
+		} else {
+			
+		}
+			
+		return "";
 	}
 }
